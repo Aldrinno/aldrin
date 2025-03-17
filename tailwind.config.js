43f41c8e429220be79de3c54ba40041c6643e19d
Aldrinno <joomortif01@gmail.com>
2025-03-17 14:33:39 +0800

    Updated files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          playfair: ["Playfair Display", "serif"],
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
